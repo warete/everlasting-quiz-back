a956da37c812e9b5da63441c166461b72be4cd74
Egor Borisovsky <egor@intervolga.ru>
2020-05-15 00:57:37 +0400

    Роуты для получения списка вопросов

--- www/index.php
+++ www/index.php
@@ -6,8 +6,28 @@ use \RedBeanPHP\R as R;
 
 $dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
 	$r->addGroup('/api', function (RouteCollector $r) {
-		$r->addRoute('GET', '/users', function () {
+		//Для всей группы возвращаем json
+		header('Content-type: application/json');
 
+		//Категории
+		$r->addGroup('/category', function (RouteCollector $r) {
+			//По id категории
+			$r->addGroup('/{category_id:\d+}', function (RouteCollector $r) {
+				//Вопросы в категории
+				$r->addGroup('/questions', function (RouteCollector $r) {
+					//Все
+					$r->addRoute('GET', '/all/', function ($categoryId) {
+						$questions = R::findAll(QUESTIONS_TABLE, 'category_id = ?', [$categoryId]);
+						echo json_encode($questions);
+					});
+					//Добавленные за последние 7 дней
+					$r->addRoute('GET', '/recently/', function ($categoryId) {
+						$startDate = (new DateTime())->modify('-7 days');
+						$questions = R::findAll(QUESTIONS_TABLE, 'category_id = ? and created >= ?', [$categoryId, $startDate->format('Y-m-d 00:00:00')]);
+						echo json_encode($questions);
+					});
+				});
+			});
 		});
 	});
 });
